a8a076ee1fa2c966159077577eb608940e5a230a
Diána Szántó <szanto.diana.1992@gmail.com>
2021-04-28 15:31:27 +0200

    asd

--- src/main/java/com/greenfox/hackathon/model/Item.java
+++ src/main/java/com/greenfox/hackathon/model/Item.java
@@ -5,7 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "items")
@@ -14,22 +22,25 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String department;
-    private String description;
-    private String linkToImage;
-    private Long cost;
-    private Long quantity;
-    @Column(name = "discounted_price")
-    private Long discountedPrice;
-    private Long discountRate = 0L;
-    @Column(name = "discounted")
-    private boolean isDiscounted = false;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String department;
+  private String description;
+  private String linkToImage;
+  private Long cost;
+  private Long quantity;
+  @Column(name = "discounted_price")
+  private Long discountedPrice;
+  private Long discountRate = 0L;
+  @Column(name = "discounted")
+  private boolean isDiscounted = false;
 
-    public Item(String name) {
-        this.name = name;
-    }
+  @ManyToMany(mappedBy = "itemList")
+  private List<Order> orderList = new ArrayList<>();
+
+  public Item(String name) {
+    this.name = name;
+  }
 }
